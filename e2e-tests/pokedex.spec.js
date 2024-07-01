10d12d632078c1191edebe2152a0f888e53cb3fe
yogajones <95644662+yogajones@users.noreply.github.com>
2024-07-01 16:14:33 +0300

    Ex 11.9

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+  })
 })
